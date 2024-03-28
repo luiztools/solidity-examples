ea061d89304d424b7420997fc0c76ccc8a06a862
LuizTools <luizfduartejr@gmail.com>
2024-03-28 19:42:46 -0300

    update hello world hardhat example

--- hello-solidity-hardhat/contracts/HelloWorld.sol
+++ hello-solidity-hardhat/contracts/HelloWorld.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
+
 contract HelloWorld {
     string public message = "Hello World!";
 
--- hello-solidity-hardhat/test/HelloWorld.test.ts
+++ hello-solidity-hardhat/test/HelloWorld.test.ts
@@ -3,19 +3,16 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("HelloWorld", () => {
-
   async function deployFixture() {
     const [owner, otherAccount] = await ethers.getSigners();
-
     const HelloWorld = await ethers.getContractFactory("HelloWorld");
     const helloWorld = await HelloWorld.deploy();
-
     return { helloWorld, owner, otherAccount };
   }
 
   it("Should Hello the world", async () => {
     const { helloWorld } = await loadFixture(deployFixture);
-
     expect(await helloWorld.helloWorld()).equal("Hello World!");
   });
+
 });
