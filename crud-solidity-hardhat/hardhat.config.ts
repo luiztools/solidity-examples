7ef9b4dadc54d7e7a7645b2c083d0728ba2bd47f
LuizTools <luizfduartejr@gmail.com>
2024-03-28 19:30:02 -0300

    hardhat deploy upgrade (Ignition)

--- crud-solidity-hardhat/hardhat.config.ts
+++ crud-solidity-hardhat/hardhat.config.ts
@@ -7,9 +7,9 @@ dotenv.config();
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
-    bsctest: {
-      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
-      chainId: 97,
+    sepolia: {
+      url: process.env.RPC_NODE,
+      chainId: 11155111,
       accounts: {
         mnemonic: process.env.SECRET
       }
---
+++ crud-solidity-hardhat/ignition/modules/BookDatabase.ts
@@ -0,0 +1,10 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const BookDatabaseModule = buildModule("BookDatabaseModule", (m) => {
+
+  const contract = m.contract("BookDatabase");
+
+  return { contract };
+});
+
+export default BookDatabaseModule;
